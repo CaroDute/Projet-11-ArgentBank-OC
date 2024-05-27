044e6f38c939f741973afdf9de7bcabc28527f8f
CaroDute <caro.duteurtre@orange.fr>
2024-05-27 12:34:38 +0200

    Ajout de Redux, dossier reducers/actions et le  store

--- main.jsx
+++ main.jsx
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/main.css";
 
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit"
+import rootReducer from "./src/reducers"
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
